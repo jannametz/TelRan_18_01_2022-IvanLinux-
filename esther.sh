886a04a4418bf9e44a2783ad9def077d512b62e3
JanaBankes <janametz@hotmail.com>
2022-08-25 01:32:51 +0200

    homework for 18.08.2022 esther

---
+++ esther.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+1. echo „Hello Student“ > /tmp/esther.txt
+2. /etc/group >> /tmp/esther.txt
+3. df -h | tail -1 >> /tmp/esther.txt 
+4. history | wc -l
+5. history > /tmp/estherhistory.txt
+6. export_file /tmp/estherhistory.txt
